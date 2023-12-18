9b8b96ffab62746a6a0954ad9ecb818dd85167c3
FraolBlk <fbf.freezy@gmail.com>
2023-12-18 12:59:38 +0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const app = express();
+const http = require('http');
+const {Server} = require('socket.io');
+const cors = require('cors');
+
+app.use(cors());
+
+const server = http.createServer(app);
+
+const io = new Server(server, {
+    cors:{
+        origin: '*',
+        methods: ['GET', 'POST'],
+    }
+});
+
+app.use("/" , (req, res) =>{
+    res.send("server started");
+})
+
+io.on("connection",(socket) =>{
+    console.log(`user connected to ${socket.id}`);
+    
+    socket.on("send_message", (message) =>{
+        console.log(message);
+        socket.broadcast.emit("received_message", message);
+    })
+})
+
+
+server.listen(3001, () => {
+    console.log('listening on http://localhost:3001');
+});
